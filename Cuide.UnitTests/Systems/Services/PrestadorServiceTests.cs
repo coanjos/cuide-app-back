113e505051b4b30aa855a705127a85f5ad8185b5
GustavodoosAnjos <anjos.gu@gmail.com>
2023-04-13 21:01:42 -0300

    adiciona teste para as services de prestadores e usuários

---
+++ Cuide.UnitTests/Systems/Services/PrestadorServiceTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuide.UnitTests.Systems.Services
+{
+    internal class PrestadorServiceTests
+    {
+    }
+}
--- Cuide.UnitTests/Systems/Services/UserServiceTests.cs
+++ Cuide.UnitTests/Systems/Services/UserServiceTests.cs
@@ -3,10 +3,109 @@ using Cuide.api.Domain.Models;
 using Cuide.api.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
+using Cuide.api.Controllers;
+using Cuide.api.Services.Interfaces;
 
 namespace Cuide.UnitTests.Systems.Services
 {
     public class UserServiceTests
     {
+        [Fact]
+
+        public async Task Post_Sucesso_DevePersistir()
+        {
+            var user = new User() { Id = 1, Email = "doguinha@doguinha", Nome = "doguinha" };
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var service = new UserService(userRepositoryMock.Object);
+            await service.PostUserAsync(user);
+            userRepositoryMock.Verify(repository => repository.PostUserAsync(user), Times.Once());
+        }
+
+        [Fact]
+
+        public async Task Get_Sucesso_DeveListar()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock.Setup(service => service.GetUsersAsync())
+                .ReturnsAsync(new List<User>()
+                {
+                    new User()
+                    {
+                        Id = 1, 
+                        Email = "doguinha@doguinha", 
+                        Nome = "doguinha"
+                    }
+                });
+            var service = new UserService(userRepositoryMock.Object);
+            var result = await service.GetUsersAsync();
+            userRepositoryMock.Verify(service => service.GetUsersAsync(), Times.Once());
+        }
+        [Fact]
+        public async Task GetId_Sucesso_DeveExecutarComSucesso()
+        {
+            int id = 1;
+
+            var userRepositoryMock = new Mock<IUserRepository>();
+            userRepositoryMock.Setup(s => s.FindUserAsync(id))
+                .ReturnsAsync(new User()
+                {
+                    Id = 1,
+                    Email = "doguinha@doguinha",
+                    Nome = "doguinha"
+                });
+
+            var service = new UserService(userRepositoryMock.Object);
+
+            var result = await service.FindUserAsync(id);
+
+            userRepositoryMock.Verify(service => service.FindUserAsync(id), Times.Once());
+            result.Should().BeOfType<User>();
+        }
+
+
+        [Fact]
+        public async Task UpdateUser_Sucesso_DeveAtualizar()
+        {
+            var id = 1;
+            var bodyToUpdate = new User() { Id = 1, Nome = "doguinha", Email = "viraaiparça@gmail.com" };
+
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            userRepositoryMock.Setup(repository => repository.FindUserAsync(id))
+                .ReturnsAsync(new User()
+                {
+                    Id = 1,
+                    Nome = "doguinha",
+                    Email = "doguinha@doguinha",
+                });
+
+            var service = new UserService(userRepositoryMock.Object);
+
+            await service.UpdateUserAsync(id, bodyToUpdate);
+
+            userRepositoryMock.Verify(repository => repository.UpdateUserAsync(id, bodyToUpdate), Times.Once());
+        }
+
+        [Fact]
+        public async Task DeleteUser_Sucesso_DeveDeletar()
+        {
+            var id = 1;
+
+            var userRepositoryMock = new Mock<IUserRepository>();
+
+            userRepositoryMock.Setup(repository => repository.FindUserAsync(id))
+                .ReturnsAsync(new User()
+                {
+                    Id = 1,
+                    Nome = "Unhas"
+                }
+              );
+
+            var service = new UserService(userRepositoryMock.Object);
+
+            await service.DeleteUserAsync(id);
+
+            userRepositoryMock.Verify(repository => repository.DeleteUserAsync(id), Times.Once());
+        }
     }
 }
