496a0316e1db7e0ed622b4d09f7849a733d24895
GustavodoosAnjos <anjos.gu@gmail.com>
2023-04-13 21:07:10 -0300

    adiciona teste para a service de prestadores

--- Cuide.UnitTests/Systems/Services/PrestadorServiceTests.cs
+++ Cuide.UnitTests/Systems/Services/PrestadorServiceTests.cs
@@ -1,12 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Cuide.api.Services;
+using Cuide.api.Domain.Models;
+using Cuide.api.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Cuide.api.Controllers;
+using Cuide.api.Services.Interfaces;
 
 namespace Cuide.UnitTests.Systems.Services
 {
-    internal class PrestadorServiceTests
+    public class PrestadorServiceTests
     {
+        [Fact]
+
+        public async Task Post_Sucesso_DevePersistir()
+        {
+            var prestador = new Prestador() { Id = 1, Nome = "doguinha" };
+            var prestadorRepositoryMock = new Mock<IPrestadorRepository>();
+            var service = new PrestadorService(prestadorRepositoryMock.Object);
+            await service.PostPrestadorAsync(prestador);
+            prestadorRepositoryMock.Verify(repository => repository.PostPrestadorAsync(prestador), Times.Once());
+        }
+
+        [Fact]
+
+        public async Task Get_Sucesso_DeveListar()
+        {
+            var prestadorRepositoryMock = new Mock<IPrestadorRepository>();
+            prestadorRepositoryMock.Setup(service => service.GetPrestadoresAsync())
+                .ReturnsAsync(new List<Prestador>()
+                {
+                    new Prestador()
+                    {
+                        Id = 1,
+                        Nome = "doguinha"
+                    }
+                });
+            var service = new PrestadorService(prestadorRepositoryMock.Object);
+            var result = await service.GetPrestadoresAsync();
+            prestadorRepositoryMock.Verify(service => service.GetPrestadoresAsync(), Times.Once());
+        }
+        [Fact]
+        public async Task GetId_Sucesso_DeveExecutarComSucesso()
+        {
+            int id = 1;
+
+            var prestadorRepositoryMock = new Mock<IPrestadorRepository>();
+            prestadorRepositoryMock.Setup(s => s.FindPrestadorAsync(id))
+                .ReturnsAsync(new Prestador()
+                {
+                    Id = 1,
+                    Nome = "doguinha"
+                });
+
+            var service = new PrestadorService(prestadorRepositoryMock.Object);
+
+            var result = await service.FindPrestadorAsync(id);
+
+            prestadorRepositoryMock.Verify(service => service.FindPrestadorAsync(id), Times.Once());
+            result.Should().BeOfType<Prestador>();
+        }
+
+
+        [Fact]
+        public async Task UpdatePrestador_Sucesso_DeveAtualizar()
+        {
+            var id = 1;
+            var bodyToUpdate = new Prestador() { Id = 1, Nome = "doguinha2" };
+
+            var prestadorRepositoryMock = new Mock<IPrestadorRepository>();
+
+            prestadorRepositoryMock.Setup(repository => repository.FindPrestadorAsync(id))
+                .ReturnsAsync(new Prestador()
+                {
+                    Id = 1,
+                    Nome = "doguinha",
+                });
+
+            var service = new PrestadorService(prestadorRepositoryMock.Object);
+
+            await service.UpdatePrestadorAsync(id, bodyToUpdate);
+
+            prestadorRepositoryMock.Verify(repository => repository.UpdatePrestadorAsync(id, bodyToUpdate), Times.Once());
+        }
+
+        [Fact]
+        public async Task DeletePrestador_Sucesso_DeveDeletar()
+        {
+            var id = 1;
+
+            var prestadorRepositoryMock = new Mock<IPrestadorRepository>();
+
+            prestadorRepositoryMock.Setup(repository => repository.FindPrestadorAsync(id))
+                .ReturnsAsync(new Prestador()
+                {
+                    Id = 1,
+                    Nome = "Unhas"
+                }
+              );
+
+            var service = new PrestadorService(prestadorRepositoryMock.Object);
+
+            await service.DeletePrestadorAsync(id);
+
+            prestadorRepositoryMock.Verify(repository => repository.DeletePrestadorAsync(id), Times.Once());
+        }
     }
 }
